31f0890f53f93ef1e98b3e941a202ca79e91786e
foxjazz <foxr4@foxjazz.net>
2021-11-20 20:57:02 -0700

    added multi word on a line

--- src/monitor.rs
+++ src/monitor.rs
@@ -13,28 +13,49 @@ pub fn start() {
         // let mut myListD = &mut myList;
         loop {
             tio::out("tm>");
-             let user = tio::readln();
-            if user == "add"{
-                addNameSecret(&mut myList);
+            let s = tio::readln();
+            let mut iterator = s.split(" ");
+            let mut resolved = false;
+            while let Some(word) = iterator.next() {
+                match word {
+                    "add" => {
+                        if let Some(second) = iterator.next()
+                        {
+                            if let Some(third) = iterator.next() {
+                                let mut data = (second.to_string(), third.to_string());
+                                myList.add(data);
+                                resolved = true;
+                                break;
+                            } else {
+                                break;
+                            }
+                        } else{
+                            break;
+                        }
+                    },
+                    _ => continue,
+                }
             }
-            if user == "list"{
-                myList.list();
+            if resolved == true{
+
+                continue;
+           }
+            else {
+                match s.as_str() {
+                    "add" => addNameSecret(&mut myList),
+                    "list" => myList.list(),
+                    "get" => {
+                        tio::out("\r\nget:");
+                        myList.get(tio::readln());
+                    },
+                    "help" => {
+                        tio::out_ln("commands: add, list, get, quit");
+                    },
+                    "quit" | "exit" => return,
+                    _ => return,
+                }
             }
-            if user == "get"{
-                tio::out("\r\nget:");
-                myList.get(tio::readln());
-            }
-            if user == "help"{
-                tio::out_ln("commands add, list, get, quit");
-            }
-            if user == "quit"{ return;}
-            /*match tio::readln().as_str() {
-                "add" => addNameSecret(&mut myList),
-                "list" => myList.list(),
-                "quit" | "exit" => return,
-                _ => return,
-            }*/
-            
+
     }
 
 }
